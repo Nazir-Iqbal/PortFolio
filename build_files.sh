98f12b57f87ce0ce2cecfd72f4daeafc73c7fc03
NazirIqbal-Solytics <nazir.iqbal@solytics-partners.com>
2026-04-17 17:40:08 +0000

    handling the compatibility issues

--- build_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Build script for Vercel - installs dependencies and collects static files.
-set -e
-
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput
