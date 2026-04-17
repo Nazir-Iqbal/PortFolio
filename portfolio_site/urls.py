c7af01329a495011ba01f1327bfdd313106b9a80
NazirIqbal-Solytics <nazir.iqbal@solytics-partners.com>
2026-04-17 20:40:48 +0000

    Fix favicon 404 by redirecting to profile image

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
+from django.templatetags.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.png', RedirectView.as_view(url=static('images/profile.jpeg'), permanent=True)),
+    path('favicon.ico', RedirectView.as_view(url=static('images/profile.jpeg'), permanent=True)),
     path('', include('core.urls')),
 ]
